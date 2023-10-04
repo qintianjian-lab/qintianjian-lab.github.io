d4918fa5e970b9a5bdbc1366549a491afce74bac
dzxrly2 <dzxrly2@gmail.com>
2023-10-04 22:23:33 +0800

    update

--- assets/vue-i18n.runtime.de203d62.js
+++ assets/vue-i18n.runtime.de203d62.js
@@ -1,4 +1,4 @@
-import{d as ct,h as Xt,e as Nn,i as gn,o as Ln,a as pn,r as Ie,c as be,w as Je,F as Ht,g as Kt,f as Tn,j as In,T as bn}from"./index.dd5cffcd.js";/*!
+import{d as ct,h as Xt,e as Nn,i as gn,o as Ln,a as pn,r as Ie,c as be,w as Je,F as Ht,g as Kt,f as Tn,j as In,T as bn}from"./index.8d622e98.js";/*!
   * shared v9.4.1
   * (c) 2023 kazuya kawaguchi
   * Released under the MIT License.
