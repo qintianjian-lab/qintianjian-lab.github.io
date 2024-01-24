606907ba4627ca6baa5684c33e7b329d697eb6b0
dzxrly2 <dzxrly2@gmail.com>
2024-01-24 10:07:26 +0800

    update page

--- assets/axios.0a1c23cd.js
+++
@@ -1,1 +0,0 @@
-import{b as i}from"./index.78ffc5eb.js";import{a}from"./axios.a596eead.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
